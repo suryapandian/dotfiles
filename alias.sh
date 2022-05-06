db7a4059a83aef71072c601b543d181fe1440a17
surya <suryamailsyou@gmail.com>
2022-05-06 09:53:12 +0100

    first commit

---
+++ alias.sh
@@ -0,0 +1,32 @@
+alias pl="/Users/suryapandian/repo/personal"
+alias life="/Users/suryapandian/repo/personal/magic"
+
+#rust
+alias cf='cargo fmt'
+alias cb=' cargo build --workspace --all-targets'
+alias cc='cargo clippy --all-targets --all-features --workspace'
+alias ct='cargo test'
+alias cr='cargo run'
+ulimit -n 24000
+export RUST_BACKTRACE=0
+
+#git
+alias gp='git pull'
+alias gm='git merge main'
+alias gpu='git push'
+alias gc='git checkout'
+alias gs='git status'
+alias gd='git diff'
+alias gcm='git commit -m'
+alias gcam='git commit -am'
+
+#k8s
+alias k='kubectl'
+alias kp='k get pods'
+alias cctxt='kubectl config view --minify'
+
+export SPACESHIP_PROMPT_SEPARATE_LINE=true
+export SPACESHIP_KUBECTL_SHOW=true
+
+
+alias s='subl .'
