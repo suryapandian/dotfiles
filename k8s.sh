8c429bbe938227fa42745f61cff447d4ce8ee17d
surya <suryamailsyou@gmail.com>
2023-03-10 09:14:39 +0000

    starship and port forwarding function

--- k8s.sh
+++ k8s.sh
@@ -16,3 +16,20 @@ alias kg="kubectl get"
 alias kc="kubectl create"
 alias kr="kubectl replace -f"
 complete -F __start_kubectl kD
+
+export d='--dry-run=client'
+export o='-o yaml'
+export do='--dry-run=client -o yaml'
+
+kpfs() {
+  local -r service="${1:-$(kubectl get service --no-headers | choose 0 | fzf)}"
+  local -r spec="$(kubectl get service "${service}" -ojson | jq ".spec")"
+  if [ "$(echo "${spec}" | jq ".ports[]" -Mc | wc -l)" -gt 1 ]; then
+    portjson="$(echo "${spec}" | jq ".ports[]" -Mc | fzf --header='pick a port object')"
+  else
+    portjson="$(echo "${spec}" | jq ".ports[0]" -Mc)"
+  fi
+  local -r port="$(echo "${portjson}" | jq ".port" -r)"
+  echo "Forwarding to svc/${service}:${port} via local 8000"
+  kubectl port-forward "svc/${service}" "8000:${port}"
+}
